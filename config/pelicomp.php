ddc57b7185e9df2e20b8377cc4d81c539a74984d
Guillaume <inscription@atlza.com>
2024-06-26 11:05:43 +0200

    Add extra parameters to custom products properties

--- config/pelicomp.php
+++ config/pelicomp.php
@@ -5,11 +5,28 @@ return [
 
     //defines properties, name and authorized values
     'properties' => [
-        'format' => [135, 120], //format can be a predefined array of values
-        'iso' => 'number', //if format is a string, it's a type for html input
-        'type' => ['Noir et blanc', 'couleur', 'couleur ciné', 'couleur reversible','panchromatique', 'panchromatique ciné', 'orthochromatique',
-            'orthopanchromatique', 'superpanchromatique', 'hyperpanchromatique', 'monochrome'],
-        'quantité' => 'number',
-        'chimie' => ['E6', 'C41', 'E6 et C41'],
+        'format' => [
+            'values' => [120, 135], //format can be a predefined array of values
+            'mandatory' => true, //format can be a predefined array of values
+        ],
+        'iso' => [
+            'values' => 'number', //if values is a string, it's a type for html input
+            'mandatory' => true,
+            'default' => null
+        ],
+        'type' => [
+            'values' => ['Noir et blanc', 'couleur', 'couleur ciné', 'couleur reversible','panchromatique', 'panchromatique ciné', 'orthochromatique',
+                'orthopanchromatique', 'superpanchromatique', 'hyperpanchromatique', 'monochrome'],
+            'mandatory' => true,
+        ],
+        'quantité' => [
+            'values' => 'number', //if values is a string, it's a type for html input
+            'mandatory' => true,
+            'default' => 1
+        ],
+        'chimie' => [
+            'values' => ['E6', 'C41', 'E6 et C41'],
+            'mandatory' => false,
+        ]
     ],
 ];
--- resources/views/components/connected/products/parts/add.blade.php
+++ resources/views/components/connected/products/parts/add.blade.php
@@ -5,28 +5,32 @@
         <form action="{{ route('manage-products-add') }}" method="post">
             @csrf
 
-            <select class="select select-bordered w-full max-w-xs mb-3" name="brand_id">
+            <select class="select select-bordered w-full max-w-xs mb-3" name="brand_id" required="required">
                 <option disabled selected>Marque</option>
                 @foreach ($brands as $brand)
                     <option value="{{ $brand->id }}" >{{ $brand->name }}</option>
                 @endforeach
             </select>
-            <input type="text" name="name" placeholder="Nom du produit" class="input input-bordered w-full max-w-xs mb-3" />
+            <input type="text" name="name" placeholder="Nom du produit" class="input input-bordered w-full max-w-xs mb-3" required="required" />
 
             @foreach( $properties as $propertyName => $propertyAttributes )
-                @if( is_array($propertyAttributes) )
-                    <select name="prop{{ $loop->iteration }}" class="select select-bordered w-full max-w-xs mb-3">
+                @if( is_array($propertyAttributes['values']) )
+                    <select name="prop{{ $loop->iteration }}"
+                            class="select select-bordered w-full max-w-xs mb-3"
+                        {!! $propertyAttributes['mandatory'] === true ? 'required="required"' : '' !!} >
                         <option disabled selected>{{ $propertyName }}</option>
-                        @foreach( $propertyAttributes as $anAttribute )
+                        @foreach( $propertyAttributes['values'] as $anAttribute )
                             <option value="{{ $anAttribute  }}">{{ $anAttribute }}</option>
                         @endforeach
                     </select>
                 @else
                     <input placeholder="{{ $propertyName }}"
-                           type="{{ $propertyAttributes }}"
+                           type="{{ $propertyAttributes['values'] }}"
                            name="prop{{ $loop->iteration }}"
                            class="input input-bordered w-full max-w-xs mb-3"
-                        {{ $propertyAttributes === "number" ? 'step="1"' : '' }} />
+                        {!! $propertyAttributes['values'] === "number" ? 'step="1"' : '' !!}
+                        {!! !empty($propertyAttributes['default']) ? 'value="'.$propertyAttributes['default'].'"' : '' !!}
+                        {!! $propertyAttributes['mandatory'] === true ? 'required="required"' : '' !!} />
                 @endif
             @endforeach
 
